081626f7a609750e9a8eec2560974b07368df651
mgawron8 <92128861+mgawron8@users.noreply.github.com>
2021-11-12 20:02:30 -0500

    deposit and withdrawal amounts changed to floats

--- src/main/java/rocks/zipcode/atm/bank/AccountData.java
+++ src/main/java/rocks/zipcode/atm/bank/AccountData.java
@@ -9,9 +9,9 @@ public final class AccountData {
     private final String name;
     private final String email;
 
-    private final int balance;
+    private final Float balance;
 
-    AccountData(int id, String name, String email, int balance) {
+    AccountData(int id, String name, String email, Float balance) {
         this.id = id;
         this.name = name;
         this.email = email;
@@ -30,7 +30,7 @@ public final class AccountData {
         return email;
     }
 
-    public int getBalance() {
+    public Float getBalance() {
         return balance;
     }
 
--- src/main/java/rocks/zipcode/atm/bank/Bank.java
+++ src/main/java/rocks/zipcode/atm/bank/Bank.java
@@ -14,23 +14,23 @@ public class Bank {
 
     public Bank() {
         accounts.put(1000, new BasicAccount(new AccountData(
-                1000, "Example 1", "example1@gmail.com", 500
+                1000, "Example 1", "example1@gmail.com", 500F
         )));
 
         accounts.put(2000, new PremiumAccount(new AccountData(
-                2000, "Example 2", "example2@gmail.com", 200
+                2000, "Example 2", "example2@gmail.com", 200F
         )));
 
         accounts.put(911, new PremiumAccount(new AccountData(
-                911, "Emergency fund", "pleaseMyFamilyIsStarving@notreal.com", -100
+                911, "Emergency fund", "pleaseMyFamilyIsStarving@notreal.com", -100F
         )));
 
         accounts.put(1, new BasicAccount(new AccountData(
-                1,"John Doe", "ThisDudeIsOld@gmail.com", 10000000
+                1,"John Doe", "ThisDudeIsOld@gmail.com", 10000000F
         )));
 
         accounts.put(2, new PremiumAccount(new AccountData(
-                2,"Theo", "iAmATodler@gmail.com", 0
+                2,"Theo", "iAmATodler@gmail.com", 0F
         )));
     }
 
@@ -44,14 +44,14 @@ public class Bank {
         }
     }
 
-    public ActionResult<AccountData> deposit(AccountData accountData, int amount) {
+    public ActionResult<AccountData> deposit(AccountData accountData, Float amount) {
         Account account = accounts.get(accountData.getId());
         account.deposit(amount);
 
         return ActionResult.success(account.getAccountData());
     }
 
-    public ActionResult<AccountData> withdraw(AccountData accountData, int amount) {
+    public ActionResult<AccountData> withdraw(AccountData accountData, Float amount) {
         Account account = accounts.get(accountData.getId());
         boolean ok = account.withdraw(amount);
 
